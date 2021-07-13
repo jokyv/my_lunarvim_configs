230a62c5a18e9b6e2b0453f9657e1b87d79ca24a
John Kyvetos <johnkyvetos@gmail.com>
2021-07-14 06:18:04 +0800

    add first configs

---
+++ lv-config.lua
@@ -0,0 +1,73 @@
+
+--[[
+O is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+-- general
+O.format_on_save = true
+O.auto_complete = true
+O.colorscheme = "spacegray"
+O.auto_close_tree = 0
+O.wrap_lines = false
+O.timeoutlen = 100
+O.leader_key = " "
+O.ignore_case = true
+O.smart_case = true
+-- have spell check on all the time
+O.default_options.spell = true
+
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+O.plugin.dashboard.active = true
+-- O.plugin.indent_line.active = true
+O.plugin.zen.active = false
+O.plugin.floatterm.active = false
+
+-- dashboard
+O.plugin.dashboard.custom_header = {""}
+-- O.plugin.dashboard.footer = {""}
+
+-- if you don't want all the parsers change this to a table of the ones you want
+O.treesitter.ensure_installed = "maintained"
+O.treesitter.ignore_install = { "haskell" }
+O.treesitter.highlight.enabled = true
+
+-- python
+-- O.python.linter = 'flake8'
+O.lang.python.isort = true
+O.lang.python.diagnostics.virtual_text = true
+O.lang.python.analysis.use_library_code_types = true
+
+-- javascript
+O.lang.tsserver.linter = nil
+
+-- TODO: add additional plugins
+-- Additional Plugins
+O.user_plugins = {
+  {"folke/todo-comments.nvim",
+    config = function()
+      require("todo-comments").setup()
+      end
+  },
+  {"norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+      end
+  }
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
+
+-- Additional Leader bindings for WhichKey
+-- O.user_which_key = {
+--   A = {
+--     name = "+Custom Leader Keys",
+--     a = { "<cmd>echo 'first custom command'<cr>", "Description for a" },
+--     b = { "<cmd>echo 'second custom command'<cr>", "Description for b" },
+--   },
+-- }
