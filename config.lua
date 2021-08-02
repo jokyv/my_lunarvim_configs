1fa8e3612973d6e61df3ee030323d671b01fd698
John Kyvetos <johnkyvetos@gmail.com>
2021-08-02 17:19:07 +0800

    using lvim instead of O

---
+++ config.lua
@@ -0,0 +1,89 @@
+-- general
+lvim.format_on_save = true
+lvim.lint_on_save = true
+lvim.colorscheme = "spacegray"
+lvim.timeoutlen = 100
+lvim.auto_close_tree = 0
+lvim.wrap_lines = true
+lvim.ignore_case = true
+lvim.smart_case = true
+
+-- have spell check on all the time
+-- lvim.default_settings.spell = true
+
+-- keymappings
+lvim.leader = "space"
+
+-- plugins
+-- dashboard settings
+lvim.builtin.dashboard.active = true
+lvim.builtin.dashboard.custom_header = { "" }
+lvim.builtin.dashboard.footer = { "" }
+
+-- terminal settings
+lvim.builtin.terminal.active = true
+
+-- nvimtree settings
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = 0
+
+-- auto-pairs
+-- lvim.builtin.auto-pairs.add_rules = ("<", ">", "lua")
+
+-- treesitter settings
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- LSP settings
+lvim.lsp.diagnostics.virtual_text = false
+
+--  TODO: add Additional Plugins
+-- #WHITE
+-- #000000
+-- Additional Plugins
+lvim.plugins = {
+	{
+		"folke/todo-comments.nvim",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+}
+
+-- python
+-- lvim.lang.python.lsp = {
+--   {
+--     exe = "jedi",
+--     args = {},
+--   },
+-- }
+
+lvim.lang.python.linters = {
+	{
+		exe = "flake8",
+		args = {},
+	},
+}
+
+lvim.lang.python.formatters = {
+	{
+		exe = "black",
+		args = {},
+	},
+}
+
+-- lua
+lvim.lang.lua.formatters = {
+	{
+		exe = "stylua",
+		args = {},
+	},
+}
--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # symlinks for lunar vim - my defaults
-rm ~/.config/nvim/lv-config.lua
-ln -s ~/projects/my_lunarvim_configs/lv-config.lua ~/.config/lvim/
+rm ~/.config/nvim/config.lua
+ln -s ~/projects/my_lunarvim_configs/config.lua ~/.config/lvim/
 ln -s ~/projects/my_lunarvim_configs/lv-user/todo-comments.lua ~/.config/lvim/lua/lv-user/
 ln -s ~/projects/my_lunarvim_configs/spell/ ~/.config/lvim/
